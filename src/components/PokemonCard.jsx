3726fc19330323cacd72e519badea8ca93f2df31
HippolyteWeber <hippolyte.weber@gmail.com>
2024-03-26 15:06:52 +0100

    feat: add condition on PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      <figcaption>Bulbasaur</figcaption>
+      <div>
+        {pokemon.imgSrc !== undefined ? (
+          <img src={pokemon.imgSrc} />
+        ) : (
+          <p>??</p>
+        )}
+      </div>
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default PokemonCard;
