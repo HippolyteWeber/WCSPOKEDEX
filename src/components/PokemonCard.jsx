80124ce80a9c1b859dc5390a83bf9c6fea096c04
HippolyteWeber <hippolyte.weber@gmail.com>
2024-03-26 16:48:30 +0100

    feat:add props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,14 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
-      <div>
-        {pokemon.imgSrc !== undefined ? (
-          <img src={pokemon.imgSrc} />
-        ) : (
-          <p>??</p>
-        )}
-      </div>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 export default PokemonCard;
