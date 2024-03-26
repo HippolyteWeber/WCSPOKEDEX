6d07c4d419782102086ec3bdc130027ad8b75ad0
HippolyteWeber <hippolyte.weber@gmail.com>
2024-03-26 17:22:50 +0100

    fix:props error

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 function PokemonCard({ pokemon }) {
   return (
     <figure>
@@ -10,5 +11,10 @@ function PokemonCard({ pokemon }) {
     </figure>
   );
 }
-
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default PokemonCard;
